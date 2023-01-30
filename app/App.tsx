d774028e5247fda9a135893f309026b6ff8d5c43
HelloWorld <sc21m2w@leeds.ac.uk>
2023-01-30 14:57:00 +0800

    modified

--- app/App.tsx
+++ app/App.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
-// import {useColorScheme} from 'react-native';
-
-import HomeScreen from './src/pages/home';
-import GalleryScreen from './src/pages/gallery';
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import Navbar from './src/components/Navbar';
-import {SignerContextProvider} from './src/context/SignerContext';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import {ScrollView, useColorScheme, View} from 'react-native';
 
-import {
-  Provider as PaperProvider,
-  useTheme,
-  MD3LightTheme,
-  adaptNavigationTheme,
-} from 'react-native-paper';
-const {LightTheme} = adaptNavigationTheme({reactNavigationLight: DefaultTheme});
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import {StatusBarComp} from './src/components/StatusBarComp';
+
+import {SignerContextProvider} from './src/context/SignerContext';
+import {PreferencesContext} from './src/context/PreferencesContext';
+
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 const GRAPH_URL = process.env.NEXT_PUBLIC_GRAPH_URL as string;
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
@@ -30,85 +17,161 @@ const apolloClient = new ApolloClient({
   uri: 'https://api.studio.thegraph.com/query/40910/archive/0.0.1',
 });
 
-const stateHandler = (prevState, newState, action) => {
-  console.log('onStateChange: ACTION:', action);
-};
+import {NavigationContainer} from '@react-navigation/native';
 
-const Stack = createStackNavigator();
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 // const Tab = createBottomTabNavigator();
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 const Tab = createMaterialBottomTabNavigator();
 
-// import {MaterialCommunityIcons} from '@expo/vector-icons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  MD2LightTheme,
+  MD2DarkTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 
 import globalStyles from './globalStyles';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {StatusBarComp} from './src/components/StatusBarComp';
+
+import GalleryScreen from './src/pages/gallery';
 import UploadScreen from './src/pages/upload';
+import SettingsScreen from './src/pages/settings';
+import lightTheme from 'web3modal/dist/themes/light';
 
 const App: any = () => {
-  // console.log('start');
-  return (
-    <PaperProvider theme={{version: 3}}>
-      {/*theme={MD3LightTheme}*/}
-      <View style={globalStyles.container}>
-        <StatusBarComp />
-        <SignerContextProvider>
-          <ApolloProvider client={apolloClient}>
-            <NavigationContainer>
-              {/*<NavigationContainer theme={LightTheme}>*/}
-              <Tab.Navigator initialRouteName="Gallery" barStyle={{height: 70}}>
-                {/*<Tab.Navigator initialRouteName="Gallery">*/}
-                <Tab.Screen
-                  name="Gallery"
-                  component={GalleryScreen}
-                  options={{
-                    // headerShown: false,
-                    title: 'Gallery',
-                    // headerTitleAlign: 'center',
-                    // headerStyle: [
-                    // useColorScheme() === 'dark' ?
-                    // ],
-                    tabBarLabel: 'Home',
-                    tabBarBadge: '6',
+  const CombinedDefaultTheme = {
+    ...MD2LightTheme,
+    ...NavigationDefaultTheme,
+    colors: {
+      ...MD2LightTheme.colors,
+      ...NavigationDefaultTheme.colors,
+    },
+  };
+  const CombinedDarkTheme = {
+    ...MD2DarkTheme,
+    ...NavigationDarkTheme,
+    colors: {
+      ...MD2DarkTheme.colors,
+      ...NavigationDarkTheme.colors,
+    },
+  };
 
-                    tabBarIcon: ({color}) => (
-                      <MaterialCommunityIcons
-                        name="home"
-                        color={color}
-                        size={20}
-                      />
-                    ),
-                  }}
-                />
-                <Tab.Screen
-                  name="Upload"
-                  component={UploadScreen}
-                  options={{
-                    // headerShown: false,
-                    title: 'Upload',
-                    // headerTitleStyle: {color: 'red'},
-                    // headerTitleAlign: 'center',
-                    // headerStyle: [
-                    // useColorScheme() === 'dark' ?
-                    // ],
-                    // headerTintColor:
-                    tabBarIcon: ({color}) => (
-                      <MaterialCommunityIcons
-                        name="upload"
-                        color={color}
-                        size={20}
-                      />
-                    ),
-                  }}
-                />
-              </Tab.Navigator>
-            </NavigationContainer>
-          </ApolloProvider>
-        </SignerContextProvider>
-      </View>
-    </PaperProvider>
+  const OverrideCombineDarkTheme = {
+    ...MD2DarkTheme,
+    ...NavigationDarkTheme,
+    colors: {
+      ...MD2DarkTheme.colors,
+      ...NavigationDarkTheme.colors,
+      primary: '#ffffff',
+    },
+  };
+
+  const [isThemeDark, setIsThemeDark] = React.useState(false);
+
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark],
+  );
+
+  return (
+    <PreferencesContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        {/*theme={MD3LightTheme}*/}
+        <View style={globalStyles.container}>
+          <StatusBarComp />
+          <SignerContextProvider>
+            <ApolloProvider client={apolloClient}>
+              <NavigationContainer theme={theme}>
+                {/*<NavigationContainer theme={LightTheme}>*/}
+                <Tab.Navigator
+                  initialRouteName="Gallery"
+                  barStyle={isThemeDark ? null : {backgroundColor: '#ffffff'}}>
+                  {/*<Tab.Navigator initialRouteName="Gallery">*/}
+                  <Tab.Screen
+                    name="Gallery"
+                    component={GalleryScreen}
+                    options={{
+                      // headerShown: false,
+                      title: 'Gallery',
+                      // headerTitleAlign: 'center',
+                      // headerStyle: [
+                      // useColorScheme() === 'dark' ?
+                      // ],
+                      tabBarLabel: 'Home',
+                      tabBarBadge: '6',
+
+                      tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons
+                          name="home"
+                          color={color}
+                          size={size ? size : 20}
+                        />
+                      ),
+                    }}
+                  />
+                  <Tab.Screen
+                    name="Upload"
+                    component={UploadScreen}
+                    options={{
+                      // headerShown: false,
+                      title: 'Upload',
+                      // headerTitleStyle: {color: 'red'},
+                      // headerTitleAlign: 'center',
+                      // headerStyle: [
+                      // useColorScheme() === 'dark' ?
+                      // ],
+                      // headerTintColor:
+                      tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons
+                          name="upload"
+                          color={color}
+                          size={size ? size : 20}
+                        />
+                      ),
+                    }}
+                  />
+                  <Tab.Screen
+                    name="Settings"
+                    component={SettingsScreen}
+                    options={{
+                      // headerShown: false,
+                      title: 'Settings',
+                      // headerTitleStyle: {color: 'red'},
+                      // headerTitleAlign: 'center',
+                      // headerStyle: [
+                      // useColorScheme() === 'dark' ?
+                      // ],
+                      // headerTintColor:
+                      tabBarIcon: ({color, size}) => (
+                        <Ionicons
+                          name="settings"
+                          color={color}
+                          size={size ? size : 18}
+                        />
+                      ),
+                    }}
+                  />
+                </Tab.Navigator>
+              </NavigationContainer>
+            </ApolloProvider>
+          </SignerContextProvider>
+        </View>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 };
 
--- app/src/pages/gallery.tsx
+++ app/src/pages/gallery.tsx
@@ -2,7 +2,12 @@ import GalleryCard from '../components/GalleryCard';
 import useSignerContext from '../context/SignerContext';
 import useArchiveMarket from '../hooks/useArchiveMarket';
 import React from 'react';
-import {View, ScrollView} from 'react-native';
+import {
+  View,
+  ScrollView,
+  RefreshControl,
+  TouchableHighlight,
+} from 'react-native';
 import PlaceHolder from '../components/Placeholder';
 // import {Box, Divider, Flex, Heading, Skeleton, Stack} from '@chakra-ui/react';
 import {convertIpfsToHttps} from '../utils/helper';
@@ -10,63 +15,48 @@ import globalStyles from '../../globalStyles';
 import {StatusBarComp} from '../components/StatusBarComp';
 // import {Provider as PaperProvider, useTheme} from 'react-native-paper';
 import {Avatar, Button, Card, Text} from 'react-native-paper';
+import {wait} from '@apollo/client/testing';
+import RNRestart from 'react-native-restart';
 export interface IGalleryPageProps {}
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />;
 
-const MyComponent = () => (
-  <Card>
-    <Card.Title
-      title="Card Title"
-      subtitle="Card Subtitle"
-      left={LeftContent}
-    />
-    <Card.Content>
-      <Text variant="titleLarge">Card title</Text>
-      <Text variant="bodyMedium">Card content</Text>
-    </Card.Content>
-    <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
-);
 function GalleryScreen(): JSX.Element {
   const {signer} = useSignerContext();
-  // const {listedNFTs} = useArchiveMarket();
   const {listedNFTs} = useArchiveMarket();
   const isLoading = signer && !listedNFTs;
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(1000).then(() => {
+      RNRestart.Restart();
+      setRefreshing(false);
+
+      // let a = useListedNFTs();
+      // navigation.navigate('Gallery');
+      // navigation.navigate('Settings');
+    });
+  }, []);
+
   return (
-    <ScrollView>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          alignItems: 'center',
-          overflow: 'scroll',
-          paddingTop: 50,
-        }}>
-        <Text>All NFTs for sale 🕶 - {listedNFTs?.length}</Text>
-        {/*<View style={{flex: 1, flexWrap: 'wrap', padding: 10}}>*/}
-        {!isLoading &&
-          listedNFTs?.map(nft => <GalleryCard key={nft.id} nft={nft} />)}
-        <Card>
-          <Card.Title
-            title="Card Title"
-            subtitle="Card Subtitle"
-            left={LeftContent}
-          />
-          <Card.Content>
-            <Text variant="titleLarge">Card title</Text>
-            <Text variant="bodyMedium">Card content</Text>
-          </Card.Content>
-          <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-          <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-          </Card.Actions>
-        </Card>
-      </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      {refreshing ? undefined : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'center',
+            overflow: 'scroll',
+            paddingTop: 20,
+          }}>
+          <Text>All NFTs for sale 🕶 - {listedNFTs?.length}</Text>
+
+          {!isLoading &&
+            listedNFTs?.map(nft => <GalleryCard key={nft.id} nft={nft} />)}
+        </View>
+      )}
     </ScrollView>
   );
 }
